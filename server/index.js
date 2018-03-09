ce13426eaf1981f0e2f1a364562c7202dd22f56b
gloompi <gloompi@geesys.ru>
2018-03-09 21:24:46 +0800

    back to origin server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const webpack = require('webpack')
-const https = require('https')
-const fs = require('fs')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const webpackHotServerMiddleware = require('webpack-hot-server-middleware')
 const clientConfig = require('../webpack/client.dev')
 const serverConfig = require('../webpack/server.dev')
 const {port} = require('../config')
-const privateKey = fs.readFileSync('/etc/nginx/ssl/cryptoinvest.key')
-const certificate = fs.readFileSync('/etc/nginx/ssl/cryptoinvest.crt')
-const credentials = {
-  key: privateKey,
-  cert: certificate
-}
 
 const app = express()
 
@@ -29,7 +21,6 @@ app.use(webpackDevMiddleware(compiler, options))
 app.use(webpackHotMiddleware(clientCompiler))
 app.use(webpackHotServerMiddleware(compiler))
 
-https.createServer(credentials, app).listen(
-  port,
-  () => console.log('started on', port)
-)
+app.listen(port, () => {
+  console.log(`Listening @ http://localhost:${port}`)
+})
