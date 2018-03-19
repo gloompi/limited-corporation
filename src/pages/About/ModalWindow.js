73ccc7d921788820a8021c29bb610eca10d90093
gloompi <gloompi@gmail.com>
2018-03-19 16:06:00 +0800

    fixed https in about

--- src/pages/About/ModalWindow.js
+++ src/pages/About/ModalWindow.js
@@ -4,7 +4,7 @@ import style from './style'
 
 const ModalWindow = ({open, handleClose, src}) => {
     return <div className={`${style.modal__wrap} ${open && style.active}`}>
-        <div className={style.modal__pic}><img src={src && src} /></div>
+        <div className={style.modal__pic}><img src={src && src.replace(/http/i, 'https')} /></div>
         <a href="" onClick={handleClose} >&times;</a>
     </div>
 }
