fad314a062764c6d2da28987c2ed23df18b5423c
gloompi <gloompi@gmail.com>
2018-04-07 14:47:49 +0800

    online chat

--- server/render.js
+++ server/render.js
@@ -51,6 +51,15 @@ export default ({ clientStats }) => (req, res) => {
           ${helmet.meta.toString()}
           ${styles}
           <meta name="theme-color" content="#ffffff">
+          <!--Start of Zendesk Chat Script-->
+          <script type="text/javascript">
+          window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+          d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+          _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+          $.src="https://v2.zopim.com/?5bWBygUl0Ee8vHr55QhWuX7ScZnMSMgl";z.t=+new Date;$.
+          type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+          </script>
+          <!--End of Zendesk Chat Script-->
         </head>
         <body>
           <script>
